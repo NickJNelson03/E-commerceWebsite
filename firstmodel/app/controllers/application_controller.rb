f2d545480199d2eb0dd997284dd459886d4d6cdc
Ubuntu <ubuntu@ip-172-31-24-241.us-east-2.compute.internal>
2024-05-05 20:32:57 +0000

    Create views for Shop

--- firstmodel/app/controllers/application_controller.rb
+++ firstmodel/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_cart
+
+  private
+
+  def set_cart
+    if user_signed_in?
+      @cart = current_user.cart
+    end
+  end
 end
