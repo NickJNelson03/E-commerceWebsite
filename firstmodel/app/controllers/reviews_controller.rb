d86dab7e17fbf599b70a12cccb5446ae86e4b33b
Braeden Singleton <157054381+Bubballoo3@users.noreply.github.com>
2024-05-09 17:10:15 -0400

    Update reviews_controller.rb

--- firstmodel/app/controllers/reviews_controller.rb
+++ firstmodel/app/controllers/reviews_controller.rb
@@ -4,7 +4,13 @@ class ReviewsController < ApplicationController
     @review= @product.reviews.create(review_params)
     redirect_to product_path(@product)
   end
+  def destroy
+    @product = Product.find(params[ :product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
 
+    redirect_to product_path(@product)
+  end
   private
     def review_params
       params.require(:review).permit( :reviewer, :stars, :content)
